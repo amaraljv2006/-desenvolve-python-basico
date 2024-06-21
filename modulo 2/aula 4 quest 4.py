90b5b7f6f0a7e8d3c762bf895cc65d6b00d0e93e
amaraljv2006 <163916224+amaraljv2006@users.noreply.github.com>
2024-06-21 18:22:29 -0300

    Create aula 4 quest 4.py

---
+++ modulo 2/aula 4 quest 4.py
@@ -0,0 +1,24 @@
+quantia = int(input("Digite o valor inteiro: "))
+
+notas100 = quantia // 100
+quantia %= 100
+
+notas50 = quantia // 50
+quantia %= 50
+
+notas20 = quantia // 20
+quantia %= 20
+
+notas10 = quantia // 10
+quantia %= 10
+
+notas5 = quantia // 5
+quantia %= 5
+
+notas2 = quantia // 2
+quantia %= 2
+
+notas1 = quantia // 1
+quantia %= 1
+
+print (f"{notas100} nota(s) de R$100,00\n{notas50} nota(s) de R$50,00\n{notas20} nota(s) de R$20,00\n{notas10} nota(s) de R$10,00\n{notas5} nota(s) de R$5,00\n{notas2} nota(s) de R$2,00\n{notas1} nota(s) de R$1,00")
